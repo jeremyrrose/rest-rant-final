9a24b7a8b5d81c4d1216d235f70b4669094fce43
Jeremy Rose <jeremy.r.rose@gmail.com>
2023-07-10 20:59:25 -0400

    Adding and rendering a new view

--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,8 @@ function home () {
     return (
       <Def>
           <main>
-              <h1>HOME</h1>
+              <h1>RESTrant</h1>
+              <h4>Sizzling hot takes!</h4>
               <div>
                 <img src="/images/HoganAndre.webp" alt="Hulk and Andre" />
               </div>
